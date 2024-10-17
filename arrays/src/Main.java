0425a7b937b0b92249a9c3013875f0d2e746cdb3
David Mauricio <117051183+DavidMauricioDev@users.noreply.github.com>
2024-10-17 20:58:06 -0300

    Add files via upload

---
+++ arrays/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        String nome= "David";
+
+        String[] nomes = {"David", "Layne", "Vitória", "Victor"};
+        int[] numeros = {7,1,5,8,4,3};
+
+        System.out.println(nomes[1]);
+        System.out.println(numeros[0]);
+
+
+
+
+    }
+}
