923a9e5e239d46448ad52c9f173b364b8d9f77e5
David Mauricio <117051183+DavidMauricioDev@users.noreply.github.com>
2024-10-17 21:59:48 -0300

    Add files via upload

---
+++ LoopsWhile/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numero = 1;
+
+        while( numero <= 50){
+            System.out.println("Numero "+ numero);
+            numero = numero +1;
+
+        }
+
+    }
+
+}
