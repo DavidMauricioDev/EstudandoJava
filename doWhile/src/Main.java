62e706ea21b92ca2886cdb98eb7dc638bdc0b8b8
David Mauricio <117051183+DavidMauricioDev@users.noreply.github.com>
2024-10-17 22:12:55 -0300

    Add files via upload

---
+++ doWhile/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        //for => para
+        //inicialização; condição; incremento
+
+
+        /*for (int i = 0; i < 6; i++) {
+            System.out.println("número " + i);
+            
+        }*/
+
+
+        int numero = 1;
+
+        while( numero <= 50) {
+            System.out.println("Numero " + numero);
+            numero = numero + 1;
+
+
+       }
+
+
+
+
+
+
+
+    }
+}
